993cdd45dbf7704ce4be5464f9433ff55db79f89
galavism <78328948+galavism@users.noreply.github.com>
2023-05-29 13:30:10 -0500

    Eliminar repo entrega 1

--- Proyecto/interaction/src/main/java/com/example/interaction/repository/questionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.interaction.repository;
-
-import com.example.interaction.entity.question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface questionRepository extends JpaRepository<question, Integer> {
-}
--- Proyecto/list-services-SOAP/src/main/java/com/diomedazo/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.diomedazo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.diomedazo.entities.Service;
-
-public interface ServiceRepository extends JpaRepository<Service, Integer>{
-}
